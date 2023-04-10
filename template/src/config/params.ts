a2ea9b020e2ff82b4790477f08a5c0981a90e346
Petr Tripolsky <tripolskypetr@gmail.com>
2023-04-10 15:24:34 +0500

    rd-inc

--- template/src/config/params.ts
+++ template/src/config/params.ts
@@ -1,3 +1,3 @@
-export const CC_CONTRACT_ADDRESS = /*process.env.REACT_APP_CONTRACT ||*/ '0xB7D4ABF464A6FD30d93bA5376aA6DaD0ec9eb6c4';
+export const CC_CONTRACT_ADDRESS = /*process.env.REACT_APP_CONTRACT ||*/ '0x57312491242A1262CECb62AA8B93FB0b0d1002a7';
 export const CC_APP_NAME = 'HashApp';
 export { default as CC_CONTRACT_ABI } from "../contract/ABI.json";
